8d220a7df213ff427f477e37f75f26aa8f0af967
artur <ArturMarschals@web.de>
2023-06-05 23:42:34 +0200

    added new error cases and refactored code

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use crate::token;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct LexerError {
     pub msg: String,
 }
@@ -11,34 +11,40 @@ impl LexerError {
 }
 
 impl LexerError {
-    pub fn unterminated_block_comment() -> Self {
-        Self::new("Unterminated block comment".to_string())
-    }
-    pub fn unknown_escape_char(c: char) -> Self {
-        Self::new(format!("Unknown escape sequence `{}`", c))
-    }
+    // mainly about chars
     pub fn empty_char_literal() -> Self {
         Self::new("Empty character literal".to_string())
-    }
-    pub fn new_line_in_char_lit() -> Self {
-        Self::new("Newline in character literal".to_string())
-    }
-    pub fn unterminated_string() -> Self {
-        Self::new("Unterminated string".to_string())
     }
     pub fn char_lit_contains_multiple_codepoints() -> Self {
         Self::new("Character literal may only contain one codepoint".to_string())
     }
 
+    pub fn unterminated_char_lit() -> Self {
+        Self::new("Unterminated character literal".to_string())
+    }
+
+    pub fn unknown_escape_char(c: char) -> Self {
+        Self::new(format!("Unknown escape sequence `{}`", c))
+    }
+    pub fn new_line_in_char_lit() -> Self {
+        Self::new("Newline in character literal".to_string())
+    }
+
+    // mainly about strings
+    pub fn unterminated_string() -> Self {
+        Self::new("Unterminated string".to_string())
+    }
+
+    // mainly about comments
+    pub fn unterminated_block_comment() -> Self {
+        Self::new("Unterminated block comment".to_string())
+    }
+
+    // mainly about numbers
     pub fn not_recognized_base_prefix(s: &str) -> Self {
         Self::new(format!("'{}' is not a recognized base prefix", s))
     }
-    pub fn unknown_character(c: char, pos: token::Position) -> Self {
-        Self::new(format!(
-            "Unknown character '{}' at {}:{}",
-            c, pos.line, pos.column
-        ))
-    }
+
     pub fn floats_dont_have_base_prefix(base: Option<token::NumberBase>) -> Self {
         LexerError::new(format!(
             "floating point literals are not allowed to have a base prefix (`{}`)",
@@ -55,6 +61,13 @@ impl LexerError {
         LexerError::new(format!(
             "invalid suffix `{}` for numeric literal",
             suffix.unwrap_or_default()
+        ))
+    }
+    // General
+    pub fn unknown_character(c: char, pos: token::Position) -> Self {
+        Self::new(format!(
+            "Unknown character '{}' at {}:{}",
+            c, pos.line, pos.column
         ))
     }
 }
