9e67fdbbdb7e2545a1944e6c51a21c0ec80a90be
Artur Marschenkulov <ArturMarschals@web.de>
2023-12-31 01:37:20 +0100

    add functions which make it possible to get the line and column number more dynamically

--- src/lexer/span.rs
+++ src/lexer/span.rs
@@ -48,3 +48,34 @@ impl From<((u32, u32), (u32, u32))> for Span {
 struct SourceMap {
     // files: HashMap<String, FileId>,
 }
+
+fn gen_newline_offsets(text: &str) -> Vec<usize> {
+    let mut offsets = Vec::new();
+
+    let mut last_offset = 0;
+    for (i, c) in text.char_indices() {
+        if c == '\n' {
+            offsets.push(i);
+        }
+        last_offset = i;
+    }
+    offsets.push(last_offset + 1);
+    offsets
+}
+
+/// Calculates the line and column based on given
+/// absolute offset and a vector of offsets which indicate the offset to a newline.
+fn get_line_and_column(newline_offsets: Vec<usize>, offset: usize) -> Option<(usize, usize)> {
+    if offset > newline_offsets.last().unwrap().clone() {
+        return None;
+    }
+    for (i, nl_offset) in newline_offsets.iter().enumerate() {
+        if offset <= *nl_offset {
+            let line = i + 1;
+            let prefix_size = if i == 0 { 0 } else { newline_offsets[i - 1] };
+            let column = offset - prefix_size;
+            return Some((line, column));
+        }
+    }
+    panic!("This should not happen {}", offset);
+}
